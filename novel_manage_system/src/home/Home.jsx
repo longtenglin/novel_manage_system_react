061225967e16912bdf6d33b9a27e26a5150a999c
unknown <2173160001355@IDM.GAD.NEC.COM.CN>
2022-05-09 18:21:16 +0800

    home-main

--- novel_manage_system/src/home/Home.jsx
+++ novel_manage_system/src/home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "../header/Header";
+import CarouselMain from "./carousel/Carousel";
+import './home.css';
 
 class Home extends React.Component{
     render() {
         return (
             <div className="novel-home">
                 <Header/>
-
+                <CarouselMain className={"home-carousel"}/>
             </div>
         )
     }
